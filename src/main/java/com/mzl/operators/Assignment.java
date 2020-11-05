09f45355ad3a36a60b02dd50f5d9755248efec42
may <34886711@qq.com>
2020-11-05 18:24:31 +0800

    1

--- src/main/java/com/mzl/operators/Assignment.java
+++ src/main/java/com/mzl/operators/Assignment.java
@@ -20,11 +20,14 @@ public class Assignment {
         t1.level = 9;
         t2.level = 47;
         System.out.println("1: t1.level: " + t1.level + ", t2.level: " + t2.level);
+        // t2 赋值给 t1，对象的赋值是引用赋值，t1 和 t2 此时指向的是堆中同一个对象（t1 原始对象的引用在 t2 赋值给其时丢失，它引用的对象会在垃圾回收时被清理）
         t1 = t2;
         System.out.println("2: t1.level: " + t1.level + ", t2.level: " + t2.level);
+        // 因为t1 t2是同一个对象，所以 t1.level 和 t2.level 实际上是
         t1.level = 27;
         System.out.println("3: t1.level: " + t1.level + ", t2.level: " + t2.level);
-        t1.level = 20;
+        // 因为t1 t2是同一个对象，所以改 t1.level 的值，再改 t2.level 的值，实际上就是覆盖了 t2的值打印出来也会是变更后的值
+        t1.level = 10;
         t2.level = 30;
         System.out.println("3: t1.level: " + t1.level + ", t2.level: " + t2.level);
     }
